118c9ba124fbd5d4d1e24ca7629e5b03ba6cb43c
irobertcostin <irobertcostin@gmail.com>
2023-01-12 23:33:47 +0200

    POST method v1

--- app/repository.js
+++ app/repository.js
@@ -263,6 +263,45 @@ export async function getAllCarsByModel(param){
     return arr;
 }
 
+
+export async function addCar(car){
+
+    let data = await getCars();
+
+    // map , filter , forEach - metode pentru arrays
+
+    let ids =   data.masini.map(e=>e.id)
+    let id = Math.floor(Math.random()*1000+1);
+
+    while(ids.includes(id)===true){
+        id = Math.floor(Math.random()*1000+1)
+    }
+
+    car.id=id;
+    data.masini.push(car);
+
+    await save(data);
+
+}
+
+export function save(data){
+
+    return new Promise((resolve,reject)={
+
+        fs.writeFile(path.resolve(__dirname,'data.json'),JSON.stringify(data),(err,data)=>{
+            if(err){
+                reject(err);
+            }else{
+                resolve();
+            }
+
+        })
+    })
+
+}
+
+
+
 //test
 // sortation
 // newest by year
